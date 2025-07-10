b267d0f07afaad41fba815de6ebf06a838a8357d
fouquetm <svplzirl@gmail.com>
2025-07-10 14:05:59 +0200

    ajout code terraform de base

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,54 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+data "azurerm_client_config" "example" {
+}
+
+data "azurerm_container_registry" "example" {
+  name                = "testacr"
+  resource_group_name = "test"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "example-continst"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "sidecar"
+    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.example.object_id
+}
