a8cd8170ddbd3bf6ef0c43c47896be07cdad0e8f
fouquetm <svplzirl@gmail.com>
2025-07-10 14:44:46 +0200

    update tf code to deploy container instance with nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,55 +9,59 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "10ce0944-5960-42ed-8657-1a8177030014"
 }
 
-data "azurerm_client_config" "example" {
+data "azurerm_container_registry" "main" {
+  name                = "acrasr243mfo"
+  resource_group_name = "rg-asr243-mfo"
 }
 
-data "azurerm_container_registry" "example" {
-  name                = "testacr"
-  resource_group_name = "test"
+data "azurerm_resource_group" "main" {
+  name     = "rg-asr243-mfo"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_user_assigned_identity" "acrpull" {
+  location            = data.azurerm_resource_group.main.location
+  name                = "uid-acrpull"
+  resource_group_name = data.azurerm_resource_group.main.name
 }
 
-resource "azurerm_container_group" "example" {
-  name                = "example-continst"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_role_assignment" "ci_acrpull" {
+  scope                = data.azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.acrpull.principal_id
+}
+
+resource "azurerm_container_group" "nginx" {
+  name                = "ci-asr243-mfo"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "ci-asr243-mfo"
   os_type             = "Linux"
+  
+  image_registry_credential {
+    user_assigned_identity_id = azurerm_user_assigned_identity.acrpull.id
+    server                    = data.azurerm_container_registry.main.login_server
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.acrpull.id]
+  }
 
   container {
-    name   = "hello-world"
-    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
-    cpu    = "0.5"
+    name   = "nginx"
+    image  = "acrasr243mfo.azurecr.io/nginx:latest"
+    cpu    = "1"
     memory = "1.5"
 
     ports {
-      port     = 443
+      port     = 80
       protocol = "TCP"
     }
   }
 
-  container {
-    name   = "sidecar"
-    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
-    cpu    = "0.5"
-    memory = "1.5"
-  }
-
-  tags = {
-    environment = "testing"
-  }
-}
-
-resource "azurerm_role_assignment" "example" {
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Reader"
-  principal_id         = data.azurerm_client_config.example.object_id
+  depends_on = [ azurerm_role_assignment.ci_acrpull ]
 }
